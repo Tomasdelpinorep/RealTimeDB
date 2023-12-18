6800b25f4a4f81e3b199ff8848cb8b5d56b11897
tomasdelpinorep <tomasdelpino28@gmail.com>
2023-12-18 13:49:07 +0100

    homework

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebaseConfig: {
+    apiKey: "AIzaSyBeGp6pQNqq91CfTx_fYyon_e0uosMPqkE",
+    authDomain: "chatqtp-81195.firebaseapp.com",
+    projectId: "chatqtp-81195",
+    storageBucket: "chatqtp-81195.appspot.com",
+    messagingSenderId: "435540055694",
+    appId: "1:435540055694:web:7da9c3b6a117366f4f9bdf",
+    measurementId: "G-92X2PD1ED4"
+  },
+};
